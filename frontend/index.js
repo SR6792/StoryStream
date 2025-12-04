0effbbaeb288527465e2e035e42e2ce31155fc39
SR6792 <sooraj.2050in@gmail.com>
2025-12-04 03:00:28 +0000

    no

--- frontend/index.js
+++ frontend/index.js
@@ -1,6 +1,6 @@
 //to get recent posts
+const a11= document.querySelector(".getpost");
 document.addEventListener("DOMContentLoaded",async(event)=>{
-    const a11= document.querySelector(".getpost");
     if(!a11) throw new Error("No div here");
     try{
         const data= await fetch('http://localhost:2000/recent');
@@ -25,10 +25,11 @@ document.addEventListener("DOMContentLoaded",async(event)=>{
         console.log(err);
     }
 });
-
-const btn = document.querySelector(".btn");
-btn.addEventListener('click',async(event)=>{
+//to get all suggestion
+const btn1 = document.querySelector(".btn1");
+btn1.addEventListener('click',async(event)=>{
     try{
+        a11.textContent='';
         const data = await fetch('http://localhost:2000/all');
         const posts = await data.json();
         posts.forEach(p=>{
@@ -40,10 +41,9 @@ btn.addEventListener('click',async(event)=>{
             `;
             post.style='justify-items:center';
             post.style.marginTop='20px';
-            post.style.width='40%';
-            post.style.height='45%';
+            post.style.width='30%';
+            post.style.height='35%';
             post.style.padding='20px';
-            post.style.marginLeft='100px';
             post.style.border='3px solid black';
             a11.appendChild(post);
         });
@@ -52,3 +52,22 @@ btn.addEventListener('click',async(event)=>{
         console.log(err);     
     }
 });
+
+//to enter a suggestion
+const btn = document.querySelector(".btn");
+const inp1=document.querySelector(".inp1");
+const inp2=document.querySelector(".inp2");
+const inp3=document.querySelector(".inp3");
+btn.addEventListener('click',async(event)=>{
+    try{
+        const name = inp1.trim.value();
+        const adm = inp2.trim.value();
+        const sug = inp3.trim.values();
+        if(!name||!adm||!sug){
+            alert('Please Fill all the Details');
+        }
+    }
+    catch(err){
+        console.log(err);
+    }
+})
